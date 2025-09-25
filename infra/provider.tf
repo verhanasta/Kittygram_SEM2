a1364b681112c8efb2745d8f40f8ac8508f93e4f
verhanasta <verhanasta@yandex.ru>
2025-09-25 23:37:59 +0300

    Test 1.7

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,10 @@
+provider "yandex" {
+  service_account_key_file = "./authorized_key.json"  # файл создается в пайплайне
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = "ru-central1-a"
+}
+
 resource "yandex_vpc_network" "vpc" {
   name        = "kittygram-network"
   description = "VPC for Kittygram"
