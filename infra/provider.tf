1d7f71295071ff057c44ebdc1ac36a39840830d0
verhanasta <verhanasta@yandex.ru>
2025-09-25 23:32:13 +0300

    Test 1.6

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = ">= 0.92.0"
-    }
-  }
-
 resource "yandex_vpc_network" "vpc" {
   name        = "kittygram-network"
   description = "VPC for Kittygram"
