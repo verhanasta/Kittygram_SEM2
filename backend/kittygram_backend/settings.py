b127a290021290b6c3999456d1959667ac6d0f6e
verhanasta <verhanasta@yandex.ru>
2025-09-29 23:17:50 +0300

    Test 1.26

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -60,7 +60,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'kittygram_db_1'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
